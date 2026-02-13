547e419fb128f2ebacd2a1c5984005c5feceb85f
VIC-OVR9000 <140988133+VIC-OVR9000@users.noreply.github.com>
2026-02-13 11:32:54 -0600

    Change index route template to 'index.html'
    Updated the index route to render 'index.html' instead of 'dashboard2.html'.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ LATINUM_STASIS = 2939.44
 
 @app.route('/')
 def index():
-    return render_template('dashboard2.html')
+    return render_template('index.html')
 
 @app.route('/api/data')
 def get_data():
@@ -67,3 +67,4 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, use_reloader=False)
+
